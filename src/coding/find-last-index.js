d9b01d882b35eb3491aee9f6f7723ffc5669672f
Pavel Gorbach <polgorbach@gmail.com>
2023-12-20 22:35:39 +0300

    feat: implement find-last-index

---
+++ src/coding/find-last-index.js
@@ -0,0 +1,29 @@
+//Implement a function findLastIndex(array, predicate, [fromIndex=array.length-1]) that takes an array of values, a function predicate, and an optional fromIndex number argument, and returns the index of the last element in the array that satisfies the provided testing function predicate. The elements of the array are iterated from right to left.
+
+/**
+ * This function returns the index of the last element in the array that satisfies the provided testing function.
+ * Otherwise, it returns -1.
+ *
+ * @param {Array} array - The array to search.
+ * @param {Function} predicate - The function invoked per iteration.
+ * @param {number} [fromIndex=array.length-1] - The index to start searching from.
+ * @returns The index of the found element, else -1.
+ */
+
+export default function findLastIndex(array, predicate, fromIndex = array.length - 1) {
+  let startIndex =
+    fromIndex < 0
+      ? Math.max(array.length + fromIndex, 0)
+      : Math.min(fromIndex, array.length - 1)
+
+  let index = startIndex
+
+  while (index >= 0) {
+    if (predicate(array[index], index, array)) {
+      return index
+    }
+    index--
+  }
+
+  return -1
+}
